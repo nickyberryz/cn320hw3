f387d95a06d1ad99fe651d1e8b097ae46d53c1f8
Computer Engineering <cn@ComClass.(none)>
2013-01-10 19:00:26 +0700

    cn320hw~ first version

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,38 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+     Movie.create!(movie)
+  end
+  #flunk "Unimplemented" # remove when done
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  #page.body.index(e1).should page.body.index(e2)
+  
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(%r{,\s*}).each_with_index {
+      |name, index|
+          step %{I check "ratings[#{name}]"}
+    }
+end
+
+Then /I should see all the movies/ do
+  Movie.find(:all).length == page.body.scan(/<tr>/).length
+end
