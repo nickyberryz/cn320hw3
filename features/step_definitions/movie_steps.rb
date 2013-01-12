49afe8e4b70c9c0cf00351cde440bdbd407aa745
Computer Engineering <cn@ComClass.(none)>
2013-01-13 01:23:41 +0700

    success

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-     Movie.create!(movie)
+
+     Movie.create!(movie) #create databae table
   end
   #flunk "Unimplemented" # remove when done
 end
@@ -15,11 +16,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  #Given %Q{I am on #{path}}!
-  #regexp = Regexp.new ".*#{e1}.*#{e2}"
-  first_pos = page.body.index(e1)
-  second_pos = page.body.index(e2)
-   p first_pos.should < second_pos
+
+  first_pos = page.body.index(e1) #find index of first movie name
+  second_pos = page.body.index(e2) #find index of second movie name
+  first_pos.should < second_pos #compare index
 
   
 end
@@ -33,19 +33,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   rating_list.split(%r{,\s*}).each do | rating |
-      rating = "ratings[" + rating + "]"
+      rating = "ratings[" + rating + "]"  #concat string ==> ratings[R]
       if uncheck
-        step %{I uncheck "#{rating}"}
+        step %{I uncheck "#{rating}"}  #do it when uncheck rating
       else
-        step %{I check "#{rating}"}
+        step %{I check "#{rating}"} #do it when check rating
       end
     end
     
 end
 
 Then /I should see all the movies/ do
-  #Movie.find(:all).length == page.body.scan(/<tr>/).length
-  #rows = Movie.all.length
-  #value = page.body.scan(/<tr>/).length
-  #rows.should == value
+  value = Movie.count(:all) +1 #+1 because table in html page have a title
+  rows = page.body.scan(/<tr>/).length
+  rows.should == value 
 end
